89edf4324084d4390a52297810b804b7e1f2d65a
s-spica <s-spica@users.noreply.github.com>
2021-01-16 23:10:35 +0900

    feat: add deps

--- mix.exs
+++ mix.exs
@@ -4,10 +4,15 @@ defmodule AbsintheExtra.MixProject do
   def project do
     [
       app: :absinthe_extra,
-      version: "0.1.0",
+      version: "0.0.1",
+      deps: deps(),
+      docs: [main: "AbsintheExtra", extras: ["README.md"]],
       elixir: "~> 1.11",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      name: "AbsintheExtra",
+      source_url: "https://github.com/s-spica/absinthe_extra",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      test_coverage: [tool: ExCoveralls]
     ]
   end
 
@@ -18,11 +23,18 @@ defmodule AbsintheExtra.MixProject do
     ]
   end
 
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:absinthe, "~> 1.6"},
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.23", only: :dev, runtime: false},
+      {:excoveralls, "~> 0.10", only: :test},
+      {:inch_ex, "~> 0.1", only: [:dev, :test], runtime: false}
     ]
   end
 end
